f5f502b36b480da075eca73b2aa835659d6081e3
EvATive7 <evative7@gmail.com>
2024-07-20 19:52:47 +0800

    feat: v1

---
+++ __init__.py
@@ -0,0 +1,87 @@
+from model import *
+from util import *
+import image
+
+
+@dataclass
+class OneBot11Config(PusherConfig):
+    pass
+
+
+@dataclass
+class Contacter:
+    id: str = ''
+    private: bool = False
+
+
+@dataclass
+class OneBot11InstanceConfig(PusherInstanceConfig):
+    image_send_method: str = 'url'  # [url, base64, file]
+    url: str = ''
+    token: str = None  # None means auth is not needed
+    contact: list[Contacter] = field(default_factory=lambda: [])
+
+
+class OneBot11(Pusher[PusherConfig, PusherInstanceConfig]):
+    async def push(self, content: Struct, to: str):
+        message_field = []
+
+        for element in content.content:
+            type_ = type(element)
+            if type_ == StructText:
+                message_field.append(
+                    {
+                        "type": "text",
+                        "data": {
+                            "text": element.text
+                        }
+                    }
+                )
+            elif type_ == StructImage:
+                imageurl = ''
+                image_send_method = self.instance_config.get('image_send_method', 'url')
+                if image_send_method == 'base64':
+                    imageurl = 'base64://' + image.image_to_base64(element.source)
+                elif image_send_method == 'url':
+                    imageurl = element.source
+                elif image_send_method == 'file':
+                    imageurl = 'file:///' + element.source
+                message_field.append(
+                    {
+                        "type": "image",
+                        "data": {
+                            "file": imageurl
+                        }
+                    }
+                )
+
+        to = self.instance_config['contact'][to]
+        private = to['private']
+        url = self.instance_config['url']
+
+        if private:
+            json_ = {
+                "user_id": int(to['id']),
+                "message": message_field
+            }
+            url = url + '/send_private_msg'
+        else:
+            json_ = {
+                "group_id": int(to['id']),
+                "message": message_field
+            }
+            url = url + '/send_group_msg'
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        if token := self.instance_config.get('token', None):
+            headers['Authorization'] = f'Bearer {token}'
+
+        response = requests.post(url, json=json_, headers=headers)
+
+        if not response.ok:
+            raise Exception()
+
+        response = response.json()
+        if response.get('status') == 'failed':
+            raise Exception()
